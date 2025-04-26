cd62579bd164b600192afaa97f1ad018795b8e74
Andreas Desmedt <andreas.desmedt@student.uantwerpen.be>
2025-04-26 16:48:29 +0200

    Try to fix celery bug

--- companions_backend/celery.py
+++ companions_backend/celery.py
@@ -1,10 +1,18 @@
 import os
+import ssl
 from celery import Celery
 
 # Set default Django settings
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'companions_backend.settings')
 
-app = Celery('companions_backend')
+app = Celery('companions_backend',
+     broker_use_ssl = {
+        'ssl_cert_reqs': ssl.CERT_NONE
+     },
+     redis_backend_use_ssl = {
+        'ssl_cert_reqs': ssl.CERT_NONE
+     }
+)
 
 # Load config from Django settings, using a CELERY namespace
 app.config_from_object('django.conf:settings', namespace='CELERY')
