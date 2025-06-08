76aaf2750c0d6b4ab613dea18219fbcf0b05d50c
Andreas Desmedt <andreas.desmedt@student.uantwerpen.be>
2025-06-08 16:50:18 +0200

    Implement Codex changes

--- lists/apps.py
+++ lists/apps.py
@@ -4,6 +4,3 @@ from django.apps import AppConfig
 class ListsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'lists'
-    
-    def ready(self):
-        import lists.signals # noqa F401
